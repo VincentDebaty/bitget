649abc880e2dc6ae812869353fd729bdbfd17584
vincentdebaty <vincent@hertbelgium.be>
2023-09-01 14:33:24 +0200

    update

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.listen(port, () => {
       }catch(e){
         console.log(e);
       }
-      run(process.env.SYMBOL,process.env.MARGIN_COIN, parseInt(process.env.MINUTES), process.env.AMOUNT, parseFloat(process.env.POURCENTAGE), parseInt(process.env.LEVERAGE));
+      run(process.env.SYMBOL,process.env.MARGIN_COIN, parseInt(process.env.MINUTES), process.env.AMOUNT, parseFloat(process.env.POURCENTAGE), parseInt(process.env.LEVERAGE), parseInt(process.env.ACTIVE));
     }, (port - 3000) * 3000);
     
 })
@@ -45,7 +45,7 @@ app.get('/test', (req, res) => {
 
 app.post('/run', (req, res) => {
     let data = req.body;
-    res.send(JSON.stringify(run(data.symbol, data.margin_coin, data.minutes, data.amount, data.pourcentage, data.leverage)));
+    res.send(JSON.stringify(run(data.symbol, data.margin_coin, data.minutes, data.amount, data.pourcentage, data.leverage, data.active)));
 })
 
 app.post('/stop', (req, res) => {
@@ -211,7 +211,7 @@ const stop = function(symbol){
   }
 }
 
-const run = async function(symbol, marginCoin, minutes, amount, pourcentage, leverage){
+const run = async function(symbol, marginCoin, minutes, amount, pourcentage, leverage, active){
   if(!currentPosition){
     currentPosition = {
       SL: 0,
@@ -290,6 +290,9 @@ const run = async function(symbol, marginCoin, minutes, amount, pourcentage, lev
         }
 
         if(positions.length == 0){
+          if(lossInARow==0 && !active){
+            return;
+          }
           currentPosition.SL = 0;
       
           if(lossInARow < maxLossInARow){
@@ -333,7 +336,7 @@ const run = async function(symbol, marginCoin, minutes, amount, pourcentage, lev
 
       if(lossInARow < maxLossInARow){
         console.log('-------');
-        setTimeout(() => run(symbol, marginCoin, minutes, amount, pourcentage, leverage), 12000);
+        setTimeout(() => run(symbol, marginCoin, minutes, amount, pourcentage, leverage, active), 12000);
       }
     }
   } catch (e) {
--- pairs.config.js
+++ pairs.config.js
@@ -5,13 +5,14 @@ module.exports = {
       script : "./index.js",
       watch  : true,
       env: {
+        ACTIVE: false,
         NODE_PORT:"3001",
         SYMBOL: "TOMOUSDT_UMCBL",
         MARGIN_COIN: "USDT",
         MINUTES: 1,
         AMOUNT: 0.5,
+        LEVERAGE: 25, // gains: 0.125
         POURCENTAGE: 1.2,
-        LEVERAGE: 25,
         RANGE_PERIOD: 240
       }
     },
@@ -20,13 +21,30 @@ module.exports = {
       script : "./index.js",
       watch  : true,
       env: {
+        ACTIVE: true,
         NODE_PORT:"3002",
         SYMBOL: "IDEXUSDT_UMCBL",
         MARGIN_COIN: "USDT",
         MINUTES: 1,
-        AMOUNT: 0.5,
-        POURCENTAGE: 1.3,
+        AMOUNT: 0.5, // gains: 0.10
         LEVERAGE: 20,
+        POURCENTAGE: 1.3,
+        RANGE_PERIOD: 240
+      }
+    },
+    {
+      name   : "cyber",
+      script : "./index.js",
+      watch  : true,
+      env: {
+        ACTIVE: true,
+        NODE_PORT:"3003",
+        SYMBOL: "IDEXUSDT_UMCBL",
+        MARGIN_COIN: "USDT",
+        MINUTES: 1,
+        AMOUNT: 0.5, // gains: 0.10
+        LEVERAGE: 20,
+        POURCENTAGE: 1.6,
         RANGE_PERIOD: 240
       }
     }
