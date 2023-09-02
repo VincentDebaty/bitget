3b3348fde5cee68d077596bde5a2c23e9a4f7213
vincentdebaty <vincent@hertbelgium.be>
2023-09-02 08:28:24 +0200

    update

--- pairs.config.js
+++ pairs.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   apps : [
-    // {
-    //   name   : "tomo",
-    //   script : "./index.js",
-    //   watch  : true,
-    //   env: {
-    //     ACTIVE: '0',
-    //     NODE_PORT:"3001",
-    //     SYMBOL: "TOMOUSDT_UMCBL",
-    //     MARGIN_COIN: "USDT",
-    //     MINUTES: 1,
-    //     AMOUNT: 0.5,
-    //     LEVERAGE: 25, // gains: 0.125
-    //     POURCENTAGE: 1.2,
-    //     RANGE_PERIOD: 240
-    //   }
-    // },
+    {
+      name   : "tomo",
+      script : "./index.js",
+      watch  : true,
+      env: {
+        ACTIVE: '0',
+        NODE_PORT:"3001",
+        SYMBOL: "TOMOUSDT_UMCBL",
+        MARGIN_COIN: "USDT",
+        MINUTES: 1,
+        AMOUNT: 0.5,
+        LEVERAGE: 25, // gains: 0.125
+        POURCENTAGE: 1.2,
+        RANGE_PERIOD: 240
+      }
+    },
     {
       name   : "idex",
       script : "./index.js",
@@ -32,21 +32,21 @@ module.exports = {
         RANGE_PERIOD: 240
       }
     },
-    {
-      name   : "cyber",
-      script : "./index.js",
-      watch  : true,
-      env: {
-        ACTIVE: '1',
-        NODE_PORT:"3003",
-        SYMBOL: "CYBERUSDT_UMCBL",
-        MARGIN_COIN: "USDT",
-        MINUTES: 1,
-        AMOUNT: 0.5, // gains: 0.10
-        LEVERAGE: 20,
-        POURCENTAGE: 1.6,
-        RANGE_PERIOD: 240
-      }
-    }
+    // {
+    //   name   : "cyber",
+    //   script : "./index.js",
+    //   watch  : true,
+    //   env: {
+    //     ACTIVE: '1',
+    //     NODE_PORT:"3003",
+    //     SYMBOL: "CYBERUSDT_UMCBL",
+    //     MARGIN_COIN: "USDT",
+    //     MINUTES: 1,
+    //     AMOUNT: 0.5, // gains: 0.10
+    //     LEVERAGE: 20,
+    //     POURCENTAGE: 1.6,
+    //     RANGE_PERIOD: 240
+    //   }
+    // }
   ]
 }
